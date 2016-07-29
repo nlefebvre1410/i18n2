986af114707389adf2ccc2e293c5568bae038cb0
kiko <nlefebre@gmx.fr>
2016-07-29 20:59:07 +0200

    c'est près

---
+++ README.md
@@ -0,0 +1,4 @@
+i18n2
+=====
+
+A Symfony project created on July 17, 2016, 10:06 pm.
---
+++ src/Cz/I18Bundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolaslefebvre
+ * Date: 29/07/2016
+ * Time: 15:26
+ */
+
+namespace Cz\I18Bundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
